b1e486884203927324c939c902540d1c72d216e4
黄俊 <993831927@qq.com>
2019-12-02 15:13:42 +0800

    'init'

---
+++ Tesseract-Captain-Server/src/main/java/tesseract/captain/server/CodeGenerator.java
@@ -0,0 +1,135 @@
+package tesseract.captain.server;
+
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 代码生成器。大家随意使用。注意下面的路径和数据库配置，表名配置
+ */
+public class CodeGenerator {
+
+    public static void main(String[] args) {
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir") + File.separator + "tesseract-admin";
+        gc.setOutputDir(projectPath + "/src/main/java");
+        gc.setAuthor("nickle");
+        gc.setOpen(false);
+        gc.setSwagger2(true);
+        gc.setBaseColumnList(true);
+        gc.setBaseResultMap(true);
+        //谨慎操作哈，这玩意儿会覆盖已创建的文件
+        //gc.setFileOverride(true);
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://localhost:3306/tesseract?useUnicode=true&useSSL=false&characterEncoding=utf8&serverTimezone=UTC");
+        // dsc.setSchemaName("public");
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("HJ.1995.11.6");
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setModuleName(null);
+        pc.setParent("admin");
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+
+        // 如果模板引擎是 freemarker
+        String templatePath = "/templates/mapper.xml.ftl";
+        // 如果模板引擎是 velocity
+        // String templatePath = "/templates/mapper.xml.vm";
+
+        // 自定义输出配置
+        List<FileOutConfig> focList = new ArrayList<>();
+        // 自定义配置会被优先输出
+        focList.add(new FileOutConfig(templatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return projectPath + "/src/main/resources/mapper/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+            }
+        });
+        /*
+        cfg.setFileCreate(new IFileCreate() {
+            @Override
+            public boolean isCreate(ConfigBuilder configBuilder, FileType fileType, String filePath) {
+                // 判断自定义文件夹是否需要创建
+                checkDir("调用默认方法创建的目录");
+                return false;
+            }
+        });
+        */
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+
+        // 配置模板
+        TemplateConfig templateConfig = new TemplateConfig();
+
+        // 配置自定义输出模板
+        //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
+        // templateConfig.setEntity("templates/entity2.java");
+        // templateConfig.setService();
+        // templateConfig.setController();
+
+        //templateConfig.setXml();
+        mpg.setTemplate(templateConfig);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        //strategy.setSuperEntityClass("com.baomidou.ant.common.BaseEntity");
+        strategy.setEntityLombokModel(true);
+        strategy.setRestControllerStyle(true);
+        // 公共父类
+        //strategy.setSuperControllerClass("com.baomidou.ant.common.BaseController");
+        // 写于父类中的公共字段
+        //strategy.setSuperEntityColumns("id");
+//        strategy.setInclude(
+//                "tesseract_job_detail",
+//                "tesseract_trigger",
+//                "tesseract_executor",
+//                "tesseract_executor_trigger_link",
+//                "tesseract_lock",
+//                "tesseract_user"
+//        );
+        //需要生成的表
+        strategy.setInclude(
+                "tesseract_menu_btn"
+//                "tesseract_menu_resource",
+//                "tesseract_role",
+//                "tesseract_role_resources",
+//                "tesseract_user_role"
+        );
+
+        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setTablePrefix(pc.getModuleName() + "_");
+        mpg.setStrategy(strategy);
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+    }
+
+}
---
+++ db/func.sql
@@ -0,0 +1,99 @@
+CREATE DATABASE if NOT EXISTS `tesseract_captain` default character set utf8 collate utf8_general_ci;
+use `tesseract_captain`;
+
+create table tesseract_captain_product
+(
+    id          int unsigned auto_increment primary key,
+    name        varchar(20) not null comment '产品名',
+    leader_id   varchar(20) not null comment '负责人id',
+    leader_name varchar(20) not null comment '负责人名字',
+    creator     varchar(20) not null comment '创建者',
+    create_time bigint      not null comment '创建时间',
+    update_time bigint      not null comment '更新时间',
+    description varchar(255) comment '描述'
+);
+
+create table tesseract_captain_user
+(
+    id           int unsigned auto_increment primary key,
+    name         varchar(20)  not null comment '用户名',
+    password     varchar(255) not null comment '用户密码',
+    creator      varchar(20)  not null comment '创建者',
+    create_time  bigint       not null comment '创建时间',
+    update_time  bigint       not null comment '更新时间',
+    description  varchar(255) comment '描述',
+    product_id   int unsigned not null comment '所属产品id',
+    product_name varchar(20)  not null comment '所属产品名字'
+);
+
+create table tesseract_captain_entity
+(
+    id           int unsigned auto_increment primary key,
+    name         varchar(20)  not null comment '实体名',
+    creator      varchar(20)  not null comment '创建者',
+    create_time  bigint       not null comment '创建时间',
+    update_time  bigint       not null comment '更新时间',
+    description  varchar(255) comment '描述',
+    product_id   int unsigned not null comment '所属产品id',
+    product_name varchar(20)  not null comment '所属产品名字'
+);
+
+create table tesseract_captain_field
+(
+    id           int unsigned auto_increment primary key,
+    entity_id    int unsigned not null comment '所属实体ID',
+    name         varchar(20)  not null comment '属性名',
+    creator      varchar(20)  not null comment '创建者',
+    create_time  bigint       not null comment '创建时间',
+    update_time  bigint       not null comment '更新时间',
+    description  varchar(255) comment '描述',
+    product_id   int unsigned not null comment '所属产品id',
+    product_name varchar(20)  not null comment '所属产品名字'
+);
+
+
+create table tesseract_captain_entity_relation
+(
+    id               int unsigned auto_increment primary key,
+    from_entity_id   int          not null comment '主实体ID',
+    from_entity_name varchar(20)  not null comment '主实体名',
+    to_entity_id     int unsigned not null comment '从实体ID',
+    to_entity_name   varchar(20)  not null comment '从实体名',
+    name             varchar(20)  not null comment '关系名',
+    creator          varchar(20)  not null comment '创建者',
+    create_time      bigint       not null comment '创建时间',
+    update_time      bigint       not null comment '更新时间',
+    description      varchar(255) comment '描述',
+    product_id       int unsigned not null comment '所属产品id',
+    product_name     varchar(20)  not null comment '所属产品名字'
+);
+
+create table tesseract_captain_field_log
+(
+    id           bigint unsigned auto_increment primary key,
+    name         varchar(20)  not null comment '属性名',
+    entity_id    int unsigned not null comment '实体ID',
+    entity_name  varchar(20)  not null comment '实体名',
+    leader_id    varchar(20)  not null comment '负责人id',
+    leader_name  varchar(20)  not null comment '负责人名字',
+    product_id   int unsigned not null comment '产品id',
+    product_name varchar(20)  not null comment '产品名',
+    creator      varchar(20)  not null comment '创建者',
+    create_time  bigint       not null comment '创建时间'
+);
+
+create table tesseract_captain_relation_log
+(
+    id               bigint unsigned auto_increment primary key,
+    from_entity_id   int          not null comment '主实体ID',
+    from_entity_name varchar(20)  not null comment '主实体名',
+    to_entity_id     int unsigned not null comment '从实体ID',
+    to_entity_name   varchar(20)  not null comment '从实体名',
+    relation_name    varchar(20)  not null comment '关系名',
+    leader_id        varchar(20)  not null comment '负责人id',
+    leader_name      varchar(20)  not null comment '负责人名字',
+    product_id       int unsigned not null comment '产品id',
+    product_name     varchar(20)  not null comment '产品名',
+    creator          varchar(20)  not null comment '创建者',
+    create_time      bigint       not null comment '创建时间'
+);
